863ba419233c38e46d5f7685556a80e2b8f43330
Keegan Crankshaw <kcranky@users.noreply.github.com>
2020-03-15 12:29:27 +0200

    Added wires and registers for standardising test bench

--- Prac4/Clock.v
+++ Prac4/Clock.v
@@ -3,24 +3,32 @@
 
 
 module WallClock(
-	// inputs
+	//inputs - these will depend on your board's constraint files
 
-	//outputs
+	//outputs - these will depend on your board's constraint files
+	
 );
 
 	//Add the reset
 
 
 	//Add and debounce the buttons
+	wire MButton;
+	wire HButton;
 	
+	// Instantiate Debounce modules here
 	
 	// registers for storing the time
-    
+    	reg [3:0]hours1=4'd0;
+	reg [3:0]hours2=4'd0;
+	reg [3:0]mins1=4'd0;
+	reg [3:0]mins1=4'd0;
     
 	//Initialize seven segment
+	// You will need to change some signals depending on you constraints
 	SS_Driver SS_Driver1(
-		Clk_100M, Reset,
-		4’d1, 4’d2, 4’d3, 4’d4, // Use temporary test values
+		<clock_signal>, <reset_signal>,
+		4’d1, 4’d2, 4’d3, 4’d4, // Use temporary test values before adding hours2, hours1, mins2, mins1
 		SegmentDrivers, SevenSegment
 	);
 	
